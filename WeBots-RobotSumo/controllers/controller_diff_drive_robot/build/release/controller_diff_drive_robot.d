442aa948f924bc0582ac7059ffc6a3d1313afe33
WizardDraelor <ewu43951@gmail.com>
2025-08-17 18:50:16 -0500

    Added differential drive controller to robot

---
+++ WeBots-RobotSumo/controllers/controller_diff_drive_robot/build/release/controller_diff_drive_robot.d
@@ -0,0 +1,6 @@
+build/release/controller_diff_drive_robot.o: \
+ controller_diff_drive_robot.c \
+ C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/motor.h
